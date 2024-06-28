806312568c9ed4e7b61d797eb34573a729caa06c
Dmitry Korolev <33462466+KorolevSoftware@users.noreply.github.com>
2024-06-28 18:21:49 +0300

    Update sokol_shader_utils.cmake

--- sokol_shader_utils.cmake
+++ sokol_shader_utils.cmake
@@ -1,12 +1,9 @@
-macro(sokol_shader shd slang)
+function(sokol_shader shd slang)
 	if(WIN32)
-		set(bin_path ${CMAKE_CURRENT_SOURCE_DIR}/bin/win32/sokol-shdc.exe)
+		set(bin_path ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/bin/win32/sokol-shdc.exe)
 	endif()
-
-	set(args "{slang: '${slang}', compiler: '${CMAKE_C_COMPILER_ID}' }")
-
 	add_custom_command(OUTPUT ${shd}.h
-		COMMAND ${bin_path} --input ${CMAKE_CURRENT_SOURCE_DIR}/${shd} --output ${CMAKE_CURRENT_BINARY_DIR}/${shd}.h --slang ${slang} --bytecode --format sokol --errfmt ${CMAKE_C_COMPILER_ID}
+		COMMAND ${bin_path} --input ${CMAKE_CURRENT_SOURCE_DIR}/${shd} --output ${CMAKE_CURRENT_BINARY_DIR}/compile_shaders/${shd}.h --slang ${slang} --bytecode --format sokol
 		COMMENT "Compile shader: ${shd}"
 		)
-endmacro()
+endfunction()
